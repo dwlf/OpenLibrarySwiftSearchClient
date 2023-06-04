81362df37d918b80d0b81e59661a3238756e9b53
Lloyd Dewolf <github@thedewofs.com>
2023-06-04 11:33:26 -0700

    findClosestBook now appears to be working
    - whether title&author, just title, or just author

--- Sources/OpenLibrarySwiftSearchClient/OpenLibrarySwiftSearchClient.swift
+++ Sources/OpenLibrarySwiftSearchClient/OpenLibrarySwiftSearchClient.swift
@@ -17,14 +17,31 @@ public struct OpenLibrarySwiftSearchClient {
 
     public init() {
     }
-        
-    static func searchBooks(query: String, completion: @escaping (Result<[OpenLibraryBook], OpenLibraryAPIError>) -> Void) {
-        guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let url = URL(string: "\(baseURL)/search.json?title=\(encodedQuery)") else {
+    
+    static func findClosestBook(title: String?, author: String?, completion: @escaping (Result<OpenLibraryBook, OpenLibraryAPIError>) -> Void) {
+        if title == nil && author == nil {
             completion(.failure(.invalidURL))
             return
         }
+
+        var urlComponents = URLComponents(string: "\(baseURL)/search.json")
+        var queryItems = [URLQueryItem(name: "limit", value: "1")] // limit the results to 1
         
+        if let title = title {
+            queryItems.append(URLQueryItem(name: "title", value: title))
+        }
+        
+        if let author = author {
+            queryItems.append(URLQueryItem(name: "author", value: author))
+        }
+        
+        urlComponents?.queryItems = queryItems
+
+        guard let url = urlComponents?.url else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 let logMessage = "Request failed: \(error.localizedDescription)"
@@ -38,59 +55,17 @@ public struct OpenLibrarySwiftSearchClient {
                 completion(.failure(.invalidResponse))
                 return
             }
+            
+            // Print raw JSON response
+            print(String(data: data, encoding: .utf8) ?? "Invalid data")
+
             
             do {
                 let decoder = JSONDecoder()
                 let searchResponse = try decoder.decode(OpenLibrarySearchResponse.self, from: data)
-                completion(.success(searchResponse.docs))
-            } catch {
-                let logMessage = "Serialization failed: \(error.localizedDescription)"
-                os_log("%@, function: %@, line: %d", log: Self.log, type: .error, logMessage, #function, #line)
-                completion(.failure(.serializationFailed))
-            }
-        }.resume()
-    }
-    
-    static func getBookDetails(olid: String, completion: @escaping (Result<OpenLibraryBookDetails, OpenLibraryAPIError>) -> Void) {
-        guard let url = URL(string: "\(baseURL)/api/books?bibkeys=OLID:\(olid)&format=json&jscmd=data") else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        let logMessage = "Fetching book details for OLID: \(olid)"
-        os_log("%@, function: %@, line: %d", log: Self.log, type: .info, logMessage, #function, #line)
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                let logMessage = "Request failed: \(error.localizedDescription)"
-                os_log("%@, function: %@, line: %d", log: Self.log, type: .error, logMessage, #function, #line)
-                completion(.failure(.requestFailed))
-                return
-            }
-            
-            guard let data = data else {
-                os_log("Invalid response data, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
-                completion(.failure(.invalidResponse))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let bookDetailsResponse = try decoder.decode([String: OpenLibraryBookDetails].self, from: data)
-                
-                if let bookDetails = bookDetailsResponse["OLID:\(olid)"] {
-                    fetchAuthorDetails(authorKeys: bookDetails.authorKey) { result in
-                        switch result {
-                        case .success(let authorDetails):
-                            var updatedBookDetails = bookDetails
-                            updatedBookDetails.author = authorDetails.name
-                            completion(.success(updatedBookDetails))
-                        case .failure(let error):
-                            completion(.failure(error))
-                        }
-                    }
+                if let firstBook = searchResponse.docs.first {
+                    completion(.success(firstBook))
                 } else {
-                    os_log("Book details serialization failed, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
                     completion(.failure(.serializationFailed))
                 }
             } catch {
@@ -100,104 +75,27 @@ public struct OpenLibrarySwiftSearchClient {
             }
         }.resume()
     }
-    
-    static func fetchAuthorDetails(authorKeys: [String]?, completion: @escaping (Result<OpenLibraryAuthorDetails, OpenLibraryAPIError>) -> Void) {
-        guard let authorKeys = authorKeys else {
-            os_log("No author keys provided, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
-            completion(.failure(.serializationFailed))
-            return
-        }
-        
-        let authorKeyString = authorKeys.joined(separator: ",")
-        let authorDetailsURL = "\(baseURL)/api/authors?keys=\(authorKeyString)&format=json"
-        
-        guard let url = URL(string: authorDetailsURL) else {
-            os_log("Invalid author details URL, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        let logMessage = "Fetching author details for keys: \(authorKeyString)"
-        os_log("%@, function: %@, line: %d", log: Self.log, type: .info, logMessage, #function, #line)
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                let logMessage = "Request failed: \(error.localizedDescription)"
-                os_log("%@, function: %@, line: %d", log: Self.log, type: .error, logMessage, #function, #line)
-                completion(.failure(.requestFailed))
-                return
-            }
-            
-            guard let data = data else {
-                os_log("Invalid response data, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
-                completion(.failure(.invalidResponse))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let authorDetailsResponse = try decoder.decode([String: OpenLibraryAuthorDetails].self, from: data)
-                
-                if let authorDetails = authorDetailsResponse.values.first {
-                    os_log("Author details fetched successfully, function: %@, line: %d", log: Self.log, type: .info, #function, #line)
-                    completion(.success(authorDetails))
-                } else {
-                    os_log("Author details serialization failed, function: %@, line: %d", log: Self.log, type: .error, #function, #line)
-                    completion(.failure(.serializationFailed))
-                }
-            } catch {
-                let logMessage = "Serialization failed: \(error.localizedDescription)"
-                os_log("%@, function: %@, line: %d", log: Self.log, type: .error, logMessage, #function, #line)
-                completion(.failure(.serializationFailed))
-            }
-        }.resume()
-    }
+
+
 }
 
 struct OpenLibraryBook: Codable {
     let key: String
     let title: String
-    let authorKey: [String]?
     let isbn: [String]?
-    let author: String?
+    let author_name: [String]?
     let url: String?
     
     enum CodingKeys: String, CodingKey {
         case key
         case title
-        case authorKey = "author_key"
         case isbn
-        case author
+        case author_name
         case url
     }
 }
 
-struct OpenLibraryBookDetails: Codable {
-    let key: String
-    let title: String
-    let authorKey: [String]?
-    let isbn: [String]?
-    var author: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case key
-        case title
-        case authorKey = "author_key"
-        case isbn
-        case author
-    }
-}
 
 struct OpenLibrarySearchResponse: Codable {
     let docs: [OpenLibraryBook]
-}
-
-struct OpenLibraryAuthorDetails: Codable {
-    let key: String
-    let name: String
-    
-    enum CodingKeys: String, CodingKey {
-        case key
-        case name
-    }
 }
--- Tests/OpenLibrarySwiftSearchClientTests/OpenLibrarySwiftSearchClientTests.swift
+++ Tests/OpenLibrarySwiftSearchClientTests/OpenLibrarySwiftSearchClientTests.swift
@@ -1,6 +1,8 @@
 import XCTest
 @testable import OpenLibrarySwiftSearchClient
 
+let openLibraryIsNotFast: TimeInterval = 30.0
+
 final class OpenLibrarySwiftSearchClientTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
@@ -8,4 +10,53 @@ final class OpenLibrarySwiftSearchClientTests: XCTestCase {
         // results.
         XCTAssertEqual(OpenLibrarySwiftSearchClient().text, "Hello, World!")
     }
+    
+    func testFindClosestBook() throws {
+        let expectation = XCTestExpectation(description: "Search for a book and return the closest match")
+
+        OpenLibrarySwiftSearchClient.findClosestBook(title: "The Da Vinci Code", author: "Dan Brown") { result in
+            switch result {
+            case .success(let book):
+                XCTAssertEqual(book.title, "The Da Vinci Code")
+                XCTAssertEqual(book.author_name?.first, "Dan Brown")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Expected success, but got \(error) instead")
+            }
+        }
+
+        wait(for: [expectation], timeout: openLibraryIsNotFast)
+    }
+    
+    func testFindClosestBookByTitle() throws {
+        let expectation = XCTestExpectation(description: "Book found")
+        OpenLibrarySwiftSearchClient.findClosestBook(title: "The Da Vinci Code", author: nil) { result in
+            switch result {
+            case .success(let book):
+                XCTAssertNotNil(book, "No book found")
+                XCTAssertEqual(book.title, "The Da Vinci Code")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("API call failed with error: \(error)")
+            }
+        }
+        wait(for: [expectation], timeout: openLibraryIsNotFast)
+    }
+
+    func testFindClosestBookByAuthor() throws {
+        let expectation = XCTestExpectation(description: "Book found")
+        OpenLibrarySwiftSearchClient.findClosestBook(title: nil, author: "Dan Brown") { result in
+            switch result {
+            case .success(let book):
+                XCTAssertNotNil(book, "No book found")
+                XCTAssertNotNil(book.author_name, "Author not found")
+                XCTAssert(book.author_name!.contains("Dan Brown"), "Author name does not contain 'Dan Brown'")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("API call failed with error: \(error)")
+            }
+        }
+        wait(for: [expectation], timeout: openLibraryIsNotFast)
+    }
+    
 }
